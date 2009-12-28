8ca3cfb59ee20acb170efc41ad194bf3238b3a39
CBE7F1F65 <CBE7F1F65@2c9a46f2-7c1c-11de-a624-ab7c8f4ec817>
2009-12-28 12:51:05 +0000

    更改场景文件

--- Resource/Script_Lua/HDSS/Scene/scene00.lua
+++ Resource/Script_Lua/HDSS/Scene/scene00.lua
@@ -16,61 +16,17 @@ function Scene_s00(playerindex, timer)
 		
 	elseif timer == 1 then
 		local col = global.ARGB(0xff, 0xffffff);
-		for i=0,1 do
-			local xoffset = i * 320;
-			hdss.Call(
-				HDSS_BGVALEX,
-				{
-					i, 0, SI_TitleScene
-				},
-				{
-					xoffset, 480, -480, 320, 480, 9000, 0, 0
-				},
-				{
-					0.008, 9000
-				}
-			)
-			hdss.Call(
-				HDSS_BGVALEX,
-				{
-					i, 1, SI_TitleScene
-				},
-				{
-					xoffset, 0, -480, 320, 480, 9000, 0, 0
-				},
-				{
-					0.008, 9000
-				}
-			)
-			hdss.Call(
-				HDSS_BGVALEX,
-				{
-					i, 2, SI_TitleScene
-				},
-				{
-					xoffset, 0, -480, 480, 480, 0, 9000, 0
-				},
-				{
-					0.008
-				}
-			)
-			hdss.Call(
-				HDSS_BGVALEX,
-				{
-					i, 3, SI_TitleScene
-				},
-				{
-					xoffset+320, 0, -480, 480, 480, 0, 9000, 0
-				},
-				{
-					0.008
-				}
-			)
-		hdssBGCOLOR(i, 0, 0, 0, col, col);
-		hdssBGCOLOR(i, 1, 0, 0, col, col);
-		hdssBGCOLOR(i, 2, 0, col, col, 0);
-		hdssBGCOLOR(i, 3, 0, col, col, 0);
-		end
+		local xoffset = playerindex * 320;
+		hdss.Call(
+			HDSS_BGVALEX,
+			{
+				playerindex, 0, SI_Scene_01_1
+			},
+			{
+				xoffset-360, 480, 0, 720, 960, -13500, 0, 0
+			}
+		)
+		hdssBGCOLOR(playerindex, 0, col, col, 0, 0);
 	end
 	
 	return true;
